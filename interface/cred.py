c57e0bc3e55e2812cfe133a5336f1c1ad813a883
Jan K. Marucha <jkmarucha@gmail.com>
2016-11-16 15:52:03 +0000

    Update cred.py

--- interface/cred.py
+++ interface/cred.py
@@ -1,4 +1,4 @@
 from hashlib import sha256
 salt = "my_salt"
-dict = {'username': sha256('password'+salt)
+dict = {'username': '07e46cdcc58c86fed4fb14b190c71bdd74fdee9a7c446893bf7e47b9a9591f11' #sha256('password'+salt).hexdigest()
 }
